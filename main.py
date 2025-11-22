f727a896948077a4b09e7ec2012b2c9a3a7583da
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-11-22 00:51:55 -0400

    add custom text prompt capability

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001", 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-        )
-    usage = response.usage_metadata
-    print(response.text)
-    print(f"Prompt tokens: {usage.prompt_token_count}")
-    print(f"Response tokens: {usage.candidates_token_count}")
+    if len(sys.argv) < 2:
+        print('Error: program was called incorrectly\nUsage: python3 main.py "Text Prompt"')
+        sys.exit(1)
+    else:
+        load_dotenv()
+        api_key = os.environ.get("GEMINI_API_KEY")
+        client = genai.Client(api_key=api_key)
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001", 
+            contents=sys.argv[1],
+            )
+        usage = response.usage_metadata
+        print(response.text)
+        print(f"Prompt tokens: {usage.prompt_token_count}")
+        print(f"Response tokens: {usage.candidates_token_count}")
 
 
 

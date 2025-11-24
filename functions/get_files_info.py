185ccb651659b06237165f441aa291b3196affcc
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-11-23 23:55:39 -0400

    add directory checking

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,14 +3,18 @@ import os
 def get_files_info(working_directory, directory="."):
     try:
         full_path = os.path.join(working_directory, directory)
-        absolute_path = os.path.abspath(directory)
-        working_directory_contents = os.listdir(working_directory)
-        if absolute_path not in working_directory_contents:
-            return f'Error: Cannot list {directory} as it is outside the permitted working directory'
+        absolute_path = os.path.abspath(full_path)
+        working_path = os.path.abspath(working_directory)
+        working_directory_contents = os.listdir(working_path)
+        directory_contents = os.listdir(absolute_path)
+        directory_info = f"Result for '{directory}' directory:\n"
+        if directory not in working_directory_contents and directory != ".":
+            return f'{directory_info}   Error: Cannot list "{directory}" as it is outside the permitted working directory'
         if not os.path.isdir(absolute_path):
-            return f'Error: {directory} is not a directory'
-        directory_info = "Result for current directory:\n"
-        for file in working_directory_contents:
+            return f'Error: "{directory}" is not a directory'
+        if directory == ".":
+            directory_info = "Result for current directory:\n"
+        for file in directory_contents:
             file_path = os.path.join(full_path, file)
             file_size = os.path.getsize(full_path)
             is_dir = os.path.isdir(file_path)

1ce3011470cfbcbe37a0cf560cb7fcdb6a0c99ba
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-11-24 17:12:26 -0400

    refactor path checking

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -5,13 +5,8 @@ def get_file_content(working_directory, file_path):
     try:
         absolute_file_path = os.path.abspath(os.path.join(working_directory, file_path))
         absolute_working_directory = os.path.abspath(working_directory)
-        working_directory_contents = os.listdir(absolute_working_directory)
-        file_in_working_directory = False
-        for file in working_directory_contents:
-            if file_path.startswith(file):
-                file_in_working_directory = True
-        if not file_in_working_directory:
-            return(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
+        if not absolute_file_path.startswith(absolute_working_directory + os.sep):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
         if not os.path.isfile(absolute_file_path):
             return(f'Error: File not found or is not a regular file: "{file_path}"')
         
